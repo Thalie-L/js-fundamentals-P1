bc1a13ffcb5452167fc50d710cd2a09f2aca4b42
“Nathalie <nathalielauture1@gmail.com>
2021-09-23 14:31:33 -0400

    Exercice 6.4 completed

--- 2-workshop/exercise-6.4.js
+++ 2-workshop/exercise-6.4.js
@@ -6,6 +6,17 @@ function exercise6_4() {
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
+    let number = 36;
+
+    while(number >= 0 )
+    {
+      if ( number % 2 === 0)
+      {
+        console.log(number);
+      }
+
+      number--;
+    }
   // -----------------------------------------------------------------
   // Write code between the lines (above)
   //
@@ -18,5 +29,5 @@ function exercise6_4() {
 
 // Once you are done type the following in the terminal to test your answer(s).
 // yarn test exercise-6.4
-
+exercise6_4();
 module.exports = exercise6_4;
